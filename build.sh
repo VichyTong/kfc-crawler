93d83e78a14d9e69cace21335ce9f3bdcd66e427
ligen131 <1353055672@qq.com>
2022-10-19 00:51:07 +0800

    :bug: bug: Fixed typo and upgrade hadoop version.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-cd woordcount
-javac -classpath $HADOOP_HOME/share/hadoop/common/hadoop-common-2.6.0.jar:$HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.6.0.jar:$HADOOP_HOME/share/hadoop/common/lib/commons-cli-1.2.jar -d classes/ src/*.java
+cd wordcount
+javac -classpath $HADOOP_HOME/share/hadoop/common/hadoop-common-3.3.4.jar:$HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-client-core-3.3.4.jar:$HADOOP_HOME/share/hadoop/common/lib/commons-cli-1.2.jar -d classes/ src/*.java
 jar -cvf wordcount.jar classes
