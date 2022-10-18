4070ea3a1c34d11af3376099adfadcf591e2e724
ligen131 <1353055672@qq.com>
2022-10-19 01:15:00 +0800

    :bug: bug: Fixed typo.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd wordcount
 javac -classpath $HADOOP_HOME/share/hadoop/common/hadoop-common-3.3.4.jar:$HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-client-core-3.3.4.jar:$HADOOP_HOME/share/hadoop/common/lib/commons-cli-1.2.jar -d classes/ src/*.java
-jar -cvf wordcount.jar classes
+jar -cvf WordCount.jar classes
 hadoop fs -rm -f -r /hbase/output/
-hadoop jar wordcount.jar com.lisong.hadoop.WordCount /hbase/input /hbase/output
+hadoop jar WordCount.jar com.lisong.hadoop.WordCount /hbase/input /hbase/output
